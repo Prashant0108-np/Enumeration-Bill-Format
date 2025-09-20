a50e8722bf9bf4fbf95f880f12416ac3153a67ac
Prashant0108-np <prasantkrchampi2001@gmail.com>
2025-09-20 22:08:35 +0530

    Resolved conflict in App.js, keep local code

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 import Dashboard from "./Dashboard";
-<<<<<<< HEAD
 import Profile from "./Components/Profile";
-
-=======
->>>>>>> b6dc7a755cdfb451d5876dedec04192cebe3abb9
 
 function App() {
   return (
@@ -15,10 +11,7 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
-<<<<<<< HEAD
       <Route path="/profile" element={<Profile />} />
-=======
->>>>>>> b6dc7a755cdfb451d5876dedec04192cebe3abb9
     </Routes>
   );
 }
