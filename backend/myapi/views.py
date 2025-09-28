407764f6a1afc6316e49abaaf3ce041771849f87
zordicharsh <bhatiharsh788@gmail.com>
2025-09-28 15:40:06 +0530

    created form page

--- backend/myapi/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-# Create yo[ur views here.
-@api_view(['GET'])
-def index(request):
-    return Response({'message':'hellow'})
