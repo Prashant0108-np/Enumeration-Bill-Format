20ce4417d6333898d3654c75545a8b729c8162ad
zordicharsh <bhatiharsh788@gmail.com>
2025-10-02 11:28:23 +0530

    Added backend setup, removed comments, and implemented API to call Firebase database

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+#send the url to exam.urls/
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('exam.urls')),  # our app routes
--- backend/exam/urls.py
+++ backend/exam/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import ExamBillView
 
+
+# get the url and decleared the class ExamBillView
 urlpatterns = [
     path("exam-bill/", ExamBillView.as_view(), name="exam-bill"),
 ]
