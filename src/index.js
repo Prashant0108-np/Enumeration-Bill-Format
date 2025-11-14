5047e6419175b69afdde0e75b7f0b8942ed6d6fd
Prashant0108-np <prasantkrchampi2001@gmail.com>
2025-11-14 09:13:13 +0530

    Documenting Done

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
+// Create the root element where the entire React application will be mounted.
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the main component wrapped inside React.StripeMode and BrowserRouter.
+// BrowserRouter enables client-side routing throughout the app.
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -14,4 +18,6 @@ root.render(
   </React.StrictMode>
 );
 
+// Optional performance measuring function.
+// Developers can log results or send them to analytics tools.
 reportWebVitals();
